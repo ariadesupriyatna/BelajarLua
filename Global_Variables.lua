47898d13f03703c31a42748b5bd8060b913716dc
code27 <88775244+ariadesupriyatna@users.noreply.github.com>
2023-02-11 13:19:03 +0700

    Add files via upload

---
+++ Global_Variables.lua
@@ -0,0 +1,13 @@
+
+melon = 17
+apel = 31
+jeruk = 46
+jumlah_1 = apel + jeruk
+jumlah_2 = melon + apel
+
+
+print("Nilai dari melon:" ,melon)
+print("Nilai dari apel:" ,apel)
+print("Nilai dari jeruk:" ,jeruk)
+print("Nilai dari apel + jeruk:" ,jumlah_1)
+print("Nilai dari melon + apel:" ,jumlah_2)
